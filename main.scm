84d96b8352e2e8872a19bb57607c497a238f3182
ian-bird <ianukusa@gmail.com>
2026-02-25 10:16:06 -0600

    bug fixes

--- main.scm
+++ main.scm
@@ -43,43 +43,57 @@
 ;;; 3) we can apply these rules inside the body of an unapplied lambda,
 ;;;    as long as we remember it's parameter is unbound.
 (define (peval-step expr env)
-  ;; if we get a symbol then look it up and return the binding
-  (cond ((symbol? expr)
-	 (cdr (assoc expr env)))
-	((number? expr) expr)
-	;; if its a lambda expression, then peval the body,
-	;; and then if it's unbound just return the var name,
-	;; otherwise return the pevaled body.
-	((and (list? expr) (eq? 'lambda (car expr)))
-	 (let ((body (peval-step (caddr expr)
-				 (acons (cadr expr)
-					(cons 'unbound (cadr expr))
-					env))))
-	   `(lambda ,(cadr expr) ,(if (unbound? body) (cdr body) body))))
-	;; if its an application, then peval the lambda.
-	;; if its unbound, then put the var name in the function call slot,
-	;; and peval the argument. Just return that, since we can't do more.
-        ((list? expr)
-	 (let ((lambda-expr (peval-step (car expr) env)))
-	   (if (unbound? lambda-expr)
-	       (list (cdr lambda-expr)
-		     (peval-step (cadr expr) env))
-	       ;; if it's not unbound, then we can do a beta reduction,
-	       ;; by replacing the application with the lambda body that has had its
-	       ;; parameter replaced with the argument.
-	       (combined-reduction (caddr lambda-expr)
-				   (cadr lambda-expr)
-				   (cadr expr)))))))
+  (let ((lambda? (lambda (x) (and (list? x) (eq? 'lambda (car x)) (= (length x) 3)))))
+    ;; if we get a symbol then look it up and return the binding
+    (cond ((symbol? expr) (cdr (assq expr env)))
+	  ((number? expr) expr)
+	  ;; if its a lambda expression, then peval the body,
+	  ;; and then if it's unbound just return the var name,
+	  ;; otherwise return the pevaled body.
+	  ((lambda? expr)
+           (let ((body (peval-step (caddr expr)
+				   (acons (cadr expr)
+					  (cons 'unbound (cadr expr))
+					  env))))
+	     `(lambda ,(cadr expr) ,(if (unbound? body) (cdr body) body))))
+	  ;; if its an application, then peval the lambda.
+	  ;; if its unbound, then put the var name in the function call slot,
+	  ;; and peval the argument. Just return that, since we can't do more.
+          ((list? expr)
+           (let ((lambda-expr (peval-step (car expr) env)))
+	     (if (unbound? lambda-expr)
+		 (list (cdr lambda-expr)
+		       (peval-step (cadr expr) env))
+		 ;; if it's not unbound, then we can do a beta reduction if its a lambda,
+		 ;; by replacing the application with the lambda body that has had its
+		 ;; parameter replaced with the argument.
+		 (if (lambda? lambda-expr)
+		     (combined-reduction (caddr lambda-expr)
+					 (cadr lambda-expr)
+					 (cadr expr))
+		     (list lambda-expr
+			   (peval-step (cadr expr) env))))))
+	  ((and (pair? expr) (eq? 'unbound (car expr)))
+	   (cdr expr)))))
 
-;; works by repeatedly performing reduction steps to a form until it doesn't change.
+;; repeatedly perform reduction steps to a form until we reach one we've seen before.
 (define (peval expr env)
-  (let ((next-step (peval-step expr env)))
-    (if (equal? next-step expr)
-	expr
-	(peval next-step env))))
+  (let loop ((expr expr)
+	     (seen (list expr)))
+    (let ((next-step (peval-step expr env)))
+      (if (member next-step seen)
+	  next-step
+	  (loop next-step (cons expr seen))))))
 
-(peval '(lambda a (lambda b  (car. ((cons. a) b))))
-       `( ,(cons 'cons. '(lambda a (lambda b (lambda c ((c a) b)))))
-	  ,(cons 'car. '(lambda c (c (lambda a (lambda b a)))))))
-
-
+;; examples:
+;; let car = \c.c(\a.\b.a)
+;; let cons = \a.\b.\s.s(a)(b)
+;; let omega = (\x.x(x))(\x.x(x))
+;;
+;; peval \a.\b.car(cons(a)(b)) => \a.\b.a
+;; peval \a.\b.car(cons(a)(omega)) => \a.\b.a
+;;
+;; let identity = \x.x
+;;
+;; peval \a.identity(a) => \a.a
+;;
